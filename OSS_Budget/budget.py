0325e687384fa0333dbc0044287cecda68e1c935
whitecity01 <kh081104@gmail.com>
2025-06-04 20:39:09 +0900

    [기능추가] 원하는 카테고리와 금액에 해당하는 지출 내역 반환 기능 추가

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -23,5 +23,22 @@ class Budget:
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
+    
+    # 카테고리에 해당하고 max_amount보다 작거나 같은 지출 목록 반환
+    def get_list_by_category_and_under_amount(self, category, max_amount):
+        category_list = self.get_category_list(category)
+        under_amount_list = self.get_list_under_amount(category_list, max_amount)
 
+        if not under_amount_list:
+            print("해당하는 조건의 지출 기록이 없습니다.")
+        else:
+            print(f"{category}에 해당하고 {max_amount} 이하의 지출 내역:\n")
+            for idx, e in enumerate(under_amount_list, 1):
+                print(f"{idx}. {e}")
+
+    def get_category_list(self, category):
+        return [e for e in self.expenses if e.category == category]
+    
+    def get_list_under_amount(self, category_list, max_amount):
+        return [e for e in category_list if e.amount <= max_amount]
 
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -10,6 +10,7 @@ def main():
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
         print("4. 종료")
+        print("5. 카테고리별 지출 내역")
         choice = input("선택 > ")
 
         if choice == "1":
@@ -31,6 +32,15 @@ def main():
         elif choice == "4":
             print("가계부를 종료합니다.")
             break
+
+        elif choice == "5":
+            category = input("원하는 카테고리 입력: ")
+            try:
+                max_amount = int(input("최대 금액 입력(원): "))
+            except ValueError:
+                print("잘못된 금액입니다.\n")
+                continue
+            budget.get_list_by_category_and_under_amount(category, max_amount)
 
         else:
             print("잘못된 선택입니다.\n")
