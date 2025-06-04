0c76392dce91c122e2890e5500aabc9537388e92
whitecity01 <kh081104@gmail.com>
2025-06-04 21:07:15 +0900

    [기능추가] 인증 시스템 구축(로그인, 회원가입)

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -4,6 +4,9 @@ from expense import Expense
 class Budget:
     def __init__(self):
         self.expenses = []
+        self.isLogin = False
+        self.id = None
+        self.password = None
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
@@ -23,5 +26,24 @@ class Budget:
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
+
+    def login(self, id, password):
+        if self.id == id and self.password == password:
+            self.isLogin = True
+            return True
+        return False
+    
+    def register(self, id, passsword):
+        if self.isLogin:
+            return False
+        else:
+            self.id = id
+            self.password = passsword
+            return True
+    
+    def is_login(self):
+        return self.isLogin
+        
+        
 
 
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -10,9 +10,15 @@ def main():
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
         print("4. 종료")
+        print("5. 로그인")
+        print("6. 회원가입")
         choice = input("선택 > ")
 
         if choice == "1":
+            if not budget.is_login():
+                print("로그인 먼저 진행해주세요.\n")
+                continue
+
             category = input("카테고리 (예: 식비, 교통 등): ")
             description = input("설명: ")
             try:
@@ -23,14 +29,43 @@ def main():
             budget.add_expense(category, description, amount)
 
         elif choice == "2":
+            if not budget.is_login():
+                print("로그인 먼저 진행해주세요.\n")
+                continue
             budget.list_expenses()
 
         elif choice == "3":
+            if not budget.is_login():
+                print("로그인 먼저 진행해주세요.\n")
+                continue
             budget.total_spent()
 
         elif choice == "4":
             print("가계부를 종료합니다.")
             break
+
+        elif choice == "5":
+            if budget.is_login():
+                print("이미 로그인 중입니다!\n")
+                continue
+
+            print("로그인을 진행합니다.\n")
+            id = input("아이디 입력: ")
+            password = input("비밀번호 입력: ")
+            if budget.login(id, password):
+                print("로그인 성공!")
+            else:
+                print("로그인 실패!")
+
+        elif choice == "6":
+            print("회원가입을 진행합니다.\n")
+            id = input("아이디 입력: ")
+            password = input("비밀번호 입력: ")
+
+            if budget.register(id, password):
+                print("회원가입 완료!")
+            else:
+                print("이미 계정이 존재합니다.")
 
         else:
             print("잘못된 선택입니다.\n")
