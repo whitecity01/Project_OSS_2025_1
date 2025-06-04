97acfed7c90585309f07704a0df07b2a9b1de519
whitecity01 <kh081104@gmail.com>
2025-06-04 20:10:41 +0900

    [기능추가] 카테고리 관리 및 검증 기능 추가

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -4,6 +4,7 @@ from expense import Expense
 class Budget:
     def __init__(self):
         self.expenses = []
+        self.categories = []
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
@@ -23,5 +24,23 @@ class Budget:
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
+    
+    def add_category(self, category):
+        if category not in self.categories:
+            self.categories.append(category)
+            print(f"카테고리에 [{category}]가 추가되었습니다!\n")
+        else:
+            print("경고: 이미 존재하는 카테고리입니다.\n")
 
-
+    def print_category_list(self):
+        print(f"현재 존재하는 카테고리: {', '.join(self.categories)}\n")
+    
+    def is_category_contain(self, category):
+        if category not in self.categories:
+            return False
+        return True
+    
+    def is_catecory_empty(self):
+        if not self.categories:
+            return True
+        return False
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -10,10 +10,21 @@ def main():
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
         print("4. 종료")
+        print("5. 카테고리 추가")
+        print("6. 카테고리 목록")
         choice = input("선택 > ")
 
         if choice == "1":
-            category = input("카테고리 (예: 식비, 교통 등): ")
+            if budget.is_catecory_empty():
+                print("경고: 현재 등록된 카테고리가 없습니다. 5번을 통해 카테고리를 추가해주십시오.\n")
+                continue
+            
+            budget.print_category_list()
+            category = input("카테고리 선택: ")
+            if not budget.is_category_contain(category):
+                print("경고: 존재하지 않는 카테고리입니다.\n")
+                continue
+
             description = input("설명: ")
             try:
                 amount = int(input("금액(원): "))
@@ -31,6 +42,13 @@ def main():
         elif choice == "4":
             print("가계부를 종료합니다.")
             break
+
+        elif choice == "5":
+            category = input("추가할 카테고리: ")
+            budget.add_category(category)
+            
+        elif choice == "6":
+            budget.print_category_list()
 
         else:
             print("잘못된 선택입니다.\n")
